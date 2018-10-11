0cd02b68577560f78256274460711e895cf81c22
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2018-10-11 13:12:12 -0400

    footer stays at bottom

--- components/footer.js
+++ components/footer.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { Box, Paragraph, Heading, Image } from 'grommet';
 
 const Footer = () => (
-  <Box>
-    <Box align='center' pad='xsmall' background='dark-2'>
+    <Box align='center' pad='xsmall' background='dark-2' >
       we got a fucking counter bois
     </Box>
-  </Box>
 );
 
 export default Footer;
--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Grommet } from 'grommet';
+import { Grommet, Box } from 'grommet';
 // import { materiallight } from 'grommet-controls/themes';
 import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
 
 const Layout = props => (
-  <Grommet>
-    <Head>
-      <title>counter</title>
-    </Head>
-    <Header />
-    {props.children}
-    <Footer />
+  <Grommet style={{ height: 'auto', minHeight: '100vh' }}>
+      <Box style={{ height: 'auto', minHeight: '100vh' }}>
+        <Head>
+          <title>counter</title>
+        </Head>
+        <Header />
+        <Box flex={true}>
+          {props.children}
+        </Box>
+        <Footer />
+      </Box>
   </Grommet>
 );
 
